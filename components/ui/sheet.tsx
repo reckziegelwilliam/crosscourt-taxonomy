6b60e6c32687d084779030cb8cd1ba39cd94864a
Liam <reckziegel.william@gmail.com>
2024-07-02 14:24:25 -0700

    chore: styling syntax logs h-X w-X to size-X

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -23,9 +23,17 @@ const portalVariants = cva("fixed inset-0 z-50 flex", {
   defaultVariants: { position: "right" },
 })
 
+type SheetProps = {
+  // position?: string;
+  className?: string;
+  // children: React.ReactNode;
+  // size?: any;
+}
+
 interface SheetPortalProps
   extends SheetPrimitive.DialogPortalProps,
-    VariantProps<typeof portalVariants> {}
+    VariantProps<typeof portalVariants>,
+    SheetProps {}
 
 const SheetPortal = ({
   position,
@@ -33,7 +41,7 @@ const SheetPortal = ({
   children,
   ...props
 }: SheetPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props}>
+  <SheetPrimitive.Portal {...props}>
     <div className={portalVariants({ position })}>{children}</div>
   </SheetPrimitive.Portal>
 )
@@ -144,12 +152,13 @@ const sheetVariants = cva(
 
 export interface DialogContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof sheetVariants>,
+    SheetProps {}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   DialogContentProps
->(({ position, size, className, children, ...props }, ref) => (
+>(({ position, size, children, className, ...props }, ref) => (
   <SheetPortal position={position}>
     <SheetOverlay />
     <SheetPrimitive.Content
@@ -159,7 +168,7 @@ const SheetContent = React.forwardRef<
     >
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
+        <X className="size-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
